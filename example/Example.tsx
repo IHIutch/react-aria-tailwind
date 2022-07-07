eb28a8ad0945db7cb0c019d001ce2ae3daeb6194
JB Hutch <jbhutch01@gmail.com>
2022-07-07 10:35:18 -0400

    Simplify examples, fix component typescript errors

--- example/Example.tsx
+++ example/Example.tsx
@@ -1,14 +1,46 @@
-import React from 'react';
-import {ExampleButton} from './Button';
-import {ExampleToggleButton} from './ToggleButton';
+import React from "react";
+import { Button } from "../src/Button";
+import { ToggleButton } from "../src/ToggleButton";
 
 export function Example() {
   return (
-    <div className="m-8 grid grid-cols-2 gap-2 max-w-sm">
+    <div className="m-8 grid max-w-sm grid-cols-2 gap-2">
       <h1 className="col-span-2">React Aria Tailwind</h1>
-      <ExampleButton>Button</ExampleButton>
-      <ExampleButton isDisabled>Button</ExampleButton>
-      <ExampleToggleButton defaultSelected>Toggle Button</ExampleToggleButton>
+      <Button
+        className="cursor-default rounded py-2 px-4 font-bold text-white transition duration-150 ease-in-out focus:outline-none"
+        defaultClassName="bg-blue-500"
+        hoverClassName="bg-blue-600"
+        activeClassName="bg-blue-700"
+        focusVisibleClassName="ring ring-offset-2 ring-blue-400"
+        disabledClassName="bg-gray-400"
+      >
+        Button
+      </Button>
+      <Button
+        className="cursor-default rounded py-2 px-4 font-bold text-white transition duration-150 ease-in-out focus:outline-none"
+        defaultClassName="bg-blue-500"
+        hoverClassName="bg-blue-600"
+        activeClassName="bg-blue-700"
+        focusVisibleClassName="ring ring-offset-2 ring-blue-400"
+        disabledClassName="bg-gray-400"
+        isDisabled
+      >
+        Button
+      </Button>
+      <ToggleButton
+        className="cursor-default rounded py-2 px-4 font-bold text-white transition duration-150 ease-in-out focus:outline-none"
+        defaultClassName="bg-blue-500"
+        hoverClassName="bg-blue-600"
+        activeClassName="bg-blue-700"
+        focusVisibleClassName="ring ring-offset-2 ring-blue-400"
+        disabledClassName="bg-gray-400"
+        selectedClassName="bg-green-600"
+        selectedHoverClassName="bg-green-700"
+        selectedActiveClassName="bg-green-800"
+        defaultSelected
+      >
+        Toggle Button
+      </ToggleButton>
     </div>
   );
 }
--- src/Button.tsx
+++ src/Button.tsx
@@ -1,40 +1,45 @@
-import {useButton, useHover, useFocusRing, mergeProps} from 'react-aria';
-import {AriaButtonProps, AriaToggleButtonProps} from '@react-types/button';
-import React, {useRef} from 'react';
-import clsx from 'clsx';
+import { useButton, useHover, useFocusRing, mergeProps } from "react-aria";
+import { AriaButtonProps } from "@react-types/button";
+import React, { useRef } from "react";
+import clsx from "clsx";
 
 interface ButtonProps extends AriaButtonProps {
-  className?: string,
-  defaultClassName?: string,
-  hoverClassName?: string,
-  activeClassName?: string,
-  focusVisibleClassName?: string,
-  disabledClassName?: string
+  className?: string;
+  defaultClassName?: string;
+  hoverClassName?: string;
+  activeClassName?: string;
+  focusVisibleClassName?: string;
+  disabledClassName?: string;
 }
 
 export function Button(props: ButtonProps) {
-  let {children, className, defaultClassName, hoverClassName, focusVisibleClassName, activeClassName, disabledClassName, isDisabled} = props;
+  let {
+    children,
+    className = "",
+    defaultClassName = "",
+    hoverClassName = "",
+    focusVisibleClassName = "",
+    activeClassName = "",
+    disabledClassName = "",
+    isDisabled = false,
+  } = props;
   let ref = useRef<HTMLButtonElement>(null);
-  let {buttonProps, isPressed} = useButton(props, ref);
-  let {hoverProps, isHovered} = useHover(props);
-  let {focusProps, isFocusVisible} = useFocusRing();
+  let { buttonProps, isPressed } = useButton(props, ref);
+  let { hoverProps, isHovered } = useHover(props);
+  let { focusProps, isFocusVisible } = useFocusRing();
 
   return (
-    <button 
+    <button
       ref={ref}
       {...mergeProps(buttonProps, hoverProps, focusProps)}
-      className={
-        clsx(
-          className,
-          {
-            [defaultClassName]: !isHovered && !isPressed,
-            [hoverClassName]: isHovered && !isPressed,
-            [activeClassName]: isPressed,
-            [focusVisibleClassName]: isFocusVisible,
-            [disabledClassName]: isDisabled
-          }
-        )
-      }>
+      className={clsx(className, {
+        [defaultClassName]: !isHovered && !isPressed,
+        [hoverClassName]: isHovered && !isPressed,
+        [activeClassName]: isPressed,
+        [focusVisibleClassName]: isFocusVisible,
+        [disabledClassName]: isDisabled,
+      })}
+    >
       {children}
     </button>
   );
